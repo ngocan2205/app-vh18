4a8ed81a52cf6b2820c00b894d919e5fb5557555
ngocan2205 <ngocan22052005@gmail.com>
2026-03-16 14:57:52 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <img src="keera.png" width={1080} height={500} />
+    <h1>Keera Mon do xao quyet</h1>
+    </>
+  );
+}
+
+export default App;
