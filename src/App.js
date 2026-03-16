8fcaddbadbeff7c7688752cb7bddd1f96d284c3c
ngocan2205 <ngocan22052005@gmail.com>
2026-03-16 15:22:06 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <>
-    <img src="keera.png" width={1080} height={500} />
+    <img src="keera.png" width={600} height={500} />
     <h1>Keera Mon do xao quyet</h1>
+    <table border={1}>
+      <tr>
+        <th>STT</th>
+        <th>Ma sinh vien</th>
+        <th>Hoa va ten</th>
+        <th>Diem</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>B23DVCN009</td>
+        <td>Nguyen Ngoc An</td>
+        <td>8.5</td>
+      </tr>
+    </table>
     </>
   );
 }
